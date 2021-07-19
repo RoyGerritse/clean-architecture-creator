b8e4e761ae572d81eecd3b77d3e99a39b38e7b2a
Roy Gerritse <roy_gerritse@hotmail.nl>
2021-07-20 00:22:18 +0200

    cac tool generator

--- Program.cs
+++ Program.cs
@@ -7,19 +7,49 @@ namespace CAC.Tool
     {
         public static void Main(string[] args)
         {
+            var versionString = Assembly.GetEntryAssembly()
+                                          .GetCustomAttribute<AssemblyInformationalVersionAttribute>()
+                                          .InformationalVersion
+                                          .ToString();
+            Console.WriteLine($"Clean Architecture Creator .NET Command-line Tools");
             if (args.Length == 0)
             {
-                var versionString = Assembly.GetEntryAssembly()
-                                        .GetCustomAttribute<AssemblyInformationalVersionAttribute>()
-                                        .InformationalVersion
-                                        .ToString();
-
-                Console.WriteLine($"cac v{versionString}");
-                Console.WriteLine("-------------");
-                Console.WriteLine("\nUsage:");
-                Console.WriteLine("  cac <message>");
+                DefaultHelp();
                 return;
             }
+
+            switch (args[0])
+            {
+                case "init":
+                    if (!string.IsNullOrEmpty(args[1]))
+                    {
+                        Console.WriteLine($"Creating setup for namespace {args[1]}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Usage: dotnet cac init [namespace]");
+                    }
+                    break;
+                case "--help":
+                case "-h":
+                    DefaultHelp();
+                    break;
+                case "--version":
+                    Console.WriteLine("Version: " + versionString);
+                    break;
+            }
+        }
+
+        private static void DefaultHelp()
+        {
+            Console.WriteLine($"Options:");
+            Console.WriteLine($"\t --version \t Show version information");
+            Console.WriteLine($"\t -h|--help \t Show help information");
+            Console.WriteLine();
+            Console.WriteLine($"Commands:");
+            Console.WriteLine($"\t init \t Initialize new Clean Architecture project.");
+            Console.WriteLine();
+            Console.WriteLine($"Use \"dotnet cac [command] --help\" for more information about a command.");
         }
     }
 }
