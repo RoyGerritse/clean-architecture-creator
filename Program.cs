c47e98316f17f5df545ecdcedebcf98824674166
Roy Gerritse <roy_gerritse@hotmail.nl>
2021-07-19 23:01:40 +0200

    Initial setup .csproj

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Reflection;
+
+namespace CAC.Tool
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                var versionString = Assembly.GetEntryAssembly()
+                                        .GetCustomAttribute<AssemblyInformationalVersionAttribute>()
+                                        .InformationalVersion
+                                        .ToString();
+
+                Console.WriteLine($"cac v{versionString}");
+                Console.WriteLine("-------------");
+                Console.WriteLine("\nUsage:");
+                Console.WriteLine("  cac <message>");
+                return;
+            }
+        }
+    }
+}
